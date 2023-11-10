f8c7f3023656549a83215554985950321b15282a
revisto <therevisto@gmail.com>
2023-11-10 15:52:26 +0330

    solve find-the-difference

---
+++ find-the-difference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        t_list = list(t)
+        for letter in s:
+            t_list.remove(letter)
+        
+        return t_list[0]
