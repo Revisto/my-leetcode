d4b5c466398354ecd4fc51a92b06d4ffb515a9e7
Revisto <theRevisto@gmail.com>
2025-01-14 18:00:44 +0330

    Add solutions for longest substring without repeating characters and symmetric tree problems

---
+++ symmetric-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+
+        def is_mirror(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+
+            return is_mirror(left.left, right.right) and is_mirror(
+                right.left, left.right
+            )
+
+        return is_mirror(root.left, root.right)
