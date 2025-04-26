20ddac395b155e1f8aaae81e6d6a60dd6bcd66b3
Revisto <theRevisto@gmail.com>
2025-04-26 22:45:45 +0330

    Rewrite twoSum solution

--- two-sum.py
+++ two-sum.py
@@ -1,15 +1,19 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_set = set(nums)
-        for num1 in nums_set:
-            ideal_num2 = target - num1
-            if ideal_num2 == num1 and nums.count(num1) == 1:
-                continue
-            if ideal_num2 in nums_set:
-                num1_index = nums.index(num1)
-                return sorted(
-                    [
-                        num1_index,
-                        nums[num1_index + 1 :].index(ideal_num2) + num1_index + 1,
-                    ]
-                )
+    def twoSum(self, nums, target: int):
+        nums_count = dict()
+        for num in nums:
+            nums_count[num] = nums_count.get(num, 0) + 1
+        
+        for num in nums_count:
+            desired_num = target - num
+            desired_num_count = nums_count.get(desired_num, 0)
+            if (desired_num == num and desired_num_count >= 2):
+                first_index = nums.index(num)
+                second_infex = nums[first_index + 1:].index(desired_num) + first_index + 1
+                return [first_index, second_infex]
+                
+            if (desired_num != num and desired_num_count >= 1):
+                first_index = nums.index(num)
+                second_infex = nums.index(desired_num)
+                return [first_index, second_infex]
+print(Solution().twoSum([3,3], 6))
